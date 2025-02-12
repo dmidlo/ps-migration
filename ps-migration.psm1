84f693f5b6244e6b15c0f60d02e236c2eef4a03a
David Midlo <dmidlo@gmail.com>
2025-02-12 11:00:45 -0600

    reorganized the repo

--- ps-migration.psm1
+++ ps-migration.psm1
@@ -1,44 +1,44 @@
 # Import Pode-related stuff:
 
-## Import Pode-Related Stuff
-## ...Pages, Widgets and Workflows I guess are views, and tasks are controllers... 
-##      seperating as much as possible; this obviously a very "in-line" framework, not to mention language.
-$viewFolders = @("pages", "widgets", "workflows")
-foreach ($viewFolder in $viewFolders) {
-    Get-ChildItem -Recurse "$PSScriptRoot\$viewFolder\*.ps1" -File | ForEach-Object {
-        . $_.FullName
-    }
-}
+# ## Import Pode-Related Stuff
+# ## ...Pages, Widgets and Workflows I guess are views, and tasks are controllers... 
+# ##      seperating as much as possible; this obviously a very "in-line" framework, not to mention language.
+# $viewFolders = @("pages", "widgets", "workflows")
+# foreach ($viewFolder in $viewFolders) {
+#     Get-ChildItem -Recurse ".\$viewFolder\*.ps1" -File | ForEach-Object {
+#         . $_.FullName
+#     }
+# }
 
-$controllerFolders = @("tasks")
-foreach ($controllerFolder in $controllerFolders) {
-    Get-ChildItem -Recurse "$PSScriptRoot\$controllerFolder\*.ps1" -File | ForEach-Object {
-        . $_.FullName
-    }
-}
+# $controllerFolders = @("tasks")
+# foreach ($controllerFolder in $controllerFolders) {
+#     Get-ChildItem -Recurse ".\$controllerFolder\*.ps1" -File | ForEach-Object {
+#         . $_.FullName
+#     }
+# }
 
 
-# Server stuff
-## bin is for stand-alone scripts and binaries
-$scriptsFolders = @("bin")
-foreach ($scriptsFolder in $scriptsFolders) {
-    Get-ChildItem -Recurse "$PSScriptRoot\$scriptsFolder\*.ps1" -File | ForEach-Object {
-        . $_.FullName
-    }
-}
+# # Server stuff
+# ## bin is for stand-alone scripts and binaries
+# $scriptsFolders = @("bin")
+# foreach ($scriptsFolder in $scriptsFolders) {
+#     Get-ChildItem -Recurse ".\$scriptsFolder\*.ps1" -File | ForEach-Object {
+#         . $_.FullName
+#     }
+# }
 
-## Database - LiteDB furnished by PSLiteDB
-$dbFolders = @("db")
-foreach ($dbFolder in $dbFolders) {
-    Get-ChildItem -Recurse "$PSScriptRoot\$dbFolder\*.ps1" -File | ForEach-Object {
-        . $_.FullName
-    }
-}
+# ## Database - LiteDB furnished by PSLiteDB
+# $dbFolders = @("db")
+# foreach ($dbFolder in $dbFolders) {
+#     Get-ChildItem -Recurse ".\$dbFolder\*.ps1" -File | ForEach-Object {
+#         . $_.FullName
+#     }
+# }
 
 # Module Utilities
 $utilitiesFolders = @("private")
 foreach ($utilitiesFolder in $utilitiesFolders) {
-    Get-ChildItem -Recurse "$PSScriptRoot\$utilitiesFolder\*.ps1" -File | ForEach-Object {
+    Get-ChildItem -Recurse ".\$utilitiesFolder\*.ps1" -File | ForEach-Object {
         . $_.FullName
     }
 }
@@ -46,7 +46,7 @@ foreach ($utilitiesFolder in $utilitiesFolders) {
 # Exported Functions
 $exportFolders = @("public")
 foreach ($exportFolder in $exportFolders) {
-    Get-ChildItem -Recurse "$PSScriptRoot\$exportFolder\*.ps1" -File | ForEach-Object {
+    Get-ChildItem -Recurse ".\$exportFolder\*.ps1" -File | ForEach-Object {
         . $_.FullName
     }
 }
@@ -54,14 +54,14 @@ foreach ($exportFolder in $exportFolders) {
 # lol
 $testFolders = @("tests")
 foreach ($testFolder in $testFolders) {
-    Get-ChildItem -Recurse "$PSScriptRoot\$testFolder\*.ps1" -File | ForEach-Object {
+    Get-ChildItem -Recurse ".\$testFolder\*.ps1" -File | ForEach-Object {
         . $_.FullName
     }
 }
 
 
 # If you have a vendored EXE in bin, you can reference it like:
-# $vendoredPath = Join-Path $PSScriptRoot 'bin/your-vendored.exe'
+# $vendoredPath = Join-Path . 'bin/your-vendored.exe'
 
 # You can also have module-level initialization code here if needed
 
