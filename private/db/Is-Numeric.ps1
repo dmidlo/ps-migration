d6d5140fd2c0994c9d3ac14fc8bdad788062bc6d
David Midlo <dmidlo@gmail.com>
2025-02-12 02:45:12 -0600

    added and tested is-numeric

---
+++ private/db/Is-Numeric.ps1
@@ -0,0 +1,15 @@
+function Is-Numeric {
+    param (
+        [string]$Value
+    )
+    
+    if (-not $Value) { return $false }  # Handle $null and empty strings
+    
+    $Value = $Value.Trim()  # Remove leading/trailing spaces
+
+    if ($Value -match '^-?\d+(\.\d+)?([eE][-+]?\d+)?$') {
+        return $true
+    }
+
+    return $false
+}
