6730a73b399630c75513c9dedbf0f2c8b38149ad
David Midlo <dmidlo@gmail.com>
2025-02-20 12:05:47 -0600

    cleaned legacy code, removed autoloading of tests, added enum

--- ps-migration.psm1
+++ ps-migration.psm1
@@ -1,39 +1,9 @@
-# Import Pode-related stuff:
-
-# ## Import Pode-Related Stuff
-# ## ...Pages, Widgets and Workflows I guess are views, and tasks are controllers... 
-# ##      seperating as much as possible; this obviously a very "in-line" framework, not to mention language.
-# $viewFolders = @("pages", "widgets", "workflows")
-# foreach ($viewFolder in $viewFolders) {
-#     Get-ChildItem -Recurse ".\$viewFolder\*.ps1" -File | ForEach-Object {
-#         . $_.FullName
-#     }
-# }
-
-# $controllerFolders = @("tasks")
-# foreach ($controllerFolder in $controllerFolders) {
-#     Get-ChildItem -Recurse ".\$controllerFolder\*.ps1" -File | ForEach-Object {
-#         . $_.FullName
-#     }
-# }
-
-
-# # Server stuff
-# ## bin is for stand-alone scripts and binaries
-# $scriptsFolders = @("bin")
-# foreach ($scriptsFolder in $scriptsFolders) {
-#     Get-ChildItem -Recurse ".\$scriptsFolder\*.ps1" -File | ForEach-Object {
-#         . $_.FullName
-#     }
-# }
-
-# ## Database - LiteDB furnished by PSLiteDB
-# $dbFolders = @("db")
-# foreach ($dbFolder in $dbFolders) {
-#     Get-ChildItem -Recurse ".\$dbFolder\*.ps1" -File | ForEach-Object {
-#         . $_.FullName
-#     }
-# }
+enum dbComponentType {
+    Component
+    Chassis
+    Module
+    Interface
+}
 
 # Module Utilities
 $utilitiesFolders = @("private")
@@ -50,20 +20,6 @@ foreach ($exportFolder in $exportFolders) {
         . $_.FullName
     }
 }
-
-# lol
-$testFolders = @("tests")
-foreach ($testFolder in $testFolders) {
-    Get-ChildItem -Recurse ".\$testFolder\*.ps1" -File | ForEach-Object {
-        . $_.FullName
-    }
-}
-
-
-# If you have a vendored EXE in bin, you can reference it like:
-# $vendoredPath = Join-Path . 'bin/your-vendored.exe'
-
-# You can also have module-level initialization code here if needed
 
 # Export public functions from this module
 Export-ModuleMember -Function * -Alias *
