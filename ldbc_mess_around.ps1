21ea839aeaa368745a15baeaa6ab185c90f21c52
David Midlo <dmidlo@gmail.com>
2025-02-26 17:27:25 -0600

    Adding Scratch workspaces to version controll

---
+++ ldbc_mess_around.ps1
@@ -0,0 +1,182 @@
+Clear-Host
+Import-Module '.\ps-migration.psd1' -Force
+# PS C:\Users\AVT.ASA\Documents\PowerShell\Modules\ps-migration> get-command -module ldbc
+# CommandType     Name                                               Version    Source
+# -----------     ----                                               -------    ------
+# Cmdlet          Invoke-LiteCommand                                 0.8.11     ldbc
+# Cmdlet          Register-LiteType                                  0.8.11     ldbc
+# Cmdlet          Remove-LiteData                                    0.8.11     ldbc
+# Cmdlet          Set-LiteData                                       0.8.11     ldbc
+# Cmdlet          Test-LiteData                                      0.8.11     ldbc
+# Cmdlet          Update-LiteData                                    0.8.11     ldbc
+# Cmdlet          Get-LiteData                                       0.8.11     ldbc
+# Cmdlet          Add-LiteData                                       0.8.11     ldbc
+# Cmdlet          Get-LiteCollection                                 0.8.11     ldbc
+
+# $Database = New-LiteDatabase :memory:
+# try {
+#     Use-LiteTransaction {
+#         $Database
+#     }
+# }
+# finally {
+#     $Database.Dispose()
+# }
+
+# [LiteDB.Collation].GetMethods() | Select-Object Name, IsStatic
+# [LiteDB.Collation] | Get-Member -MemberType Method -Force
+
+# $connectionString = [LiteDB.ConnectionString]::New()
+# $connectionString | Get-Member -MemberType Method -Force
+# $connectionString.set_AutoRebuild($true)
+# $connectionString.get_AutoRebuild() 
+# [LiteDB.Collation].GetConstructors()
+# [LiteDB.Collation].GetConstructors() | ForEach-Object { $_.GetParameters() }
+# [LiteDB.Collation].GetConstructors() | ForEach-Object { 
+#     "Constructor: " + $_.ToString()
+# }
+
+# $LocaleId = [System.Globalization.CultureInfo]::GetCultureInfo("en-US")
+# $IgnoreCase = [System.Globalization.CompareOptions]::IgnoreCase
+# $IgnoreNonSpace = [System.Globalization.CompareOptions]::IgnoreNonSpace
+# $IgnoreSymbols = [System.Globalization.CompareOptions]::IgnoreSymbols
+# $connectionString.set_Collation([LiteDB.Collation]::New($LocaleId.LCID, ($IgnoreCase -bor $IgnoreNonSpace -bor $IgnoreSymbols)))
+
+
+# [LiteDB.ConnectionType]::Shared
+# [LiteDB.ConnectionType]::Direct
+# $connectionString.set_Connection([LiteDB.ConnectionType]::Shared)
+# $connectionString.set_Filename(".\StoredObjects\ps-migration.db")
+
+# $Database = New-LiteDatabase -ConnectionString $connectionString
+# try {
+#     Use-LiteTransaction {
+#         $Database
+#     }
+# }
+# finally {
+#     $Database.Dispose()
+# }
+
+# Write-Host $connectionString.GetType()
+
+# Use-LiteDatabase -ConnectionString $connectionString {
+#         # $connectionString
+#         # Invoke-LiteCommand 'pragma UTC_DATE = true;'
+#         $pragmas = (Invoke-LiteCommand 'select pragmas from $database;' -As PS).pragmas
+#         $pragmas | Get-Member
+# }
+
+# $dbPassword = Get-StoredCredential -CredentialName "dbPassword" -Path ".\StoredObjects\Credentials\"
+
+# PS C:\Users\AVT.ASA\Documents\PowerShell\Modules\ps-migration> get-help Get-LiteCollection -Full 
+
+# NAME
+#     Get-LiteCollection
+
+# SYNOPSIS
+#     Gets the collection instance.
+
+
+# SYNTAX
+#     Get-LiteCollection [-CollectionName] <String> [[-AutoId] <BsonAutoId>] [-Database <ILiteDatabase>] [<CommonParameters>]
+
+
+# DESCRIPTION
+#     The cmdlet gets the collection instance by its name from the specified or default database.
+
+
+# PARAMETERS
+#     -CollectionName
+#         The collection name, case insensitive.
+
+#         Required?                    true
+#         Position?                    0
+#         Default value
+#         Accept pipeline input?
+#         Aliases
+#         Accept wildcard characters?
+
+#     -AutoId
+#         The automatic identifier data type.
+
+#         Values : Int32, Int64, ObjectId, Guid
+
+#         Required?                    false
+#         Position?                    1
+#         Default value
+#         Accept pipeline input?
+#         Aliases
+#         Accept wildcard characters?
+
+#     -Database
+#         The database instance. If it is omitted then the variable $Database is expected. Use New-LiteDatabase or Use-LiteDatabase in order to get the database instance.
+
+#         Required?                    false
+#         Position?                    named
+#         Default value
+#         Accept pipeline input?
+#         Aliases
+#         Accept wildcard characters?
+
+#     <CommonParameters>
+#         This cmdlet supports the common parameters: Verbose, Debug,
+#         ErrorAction, ErrorVariable, WarningAction, WarningVariable,
+#         OutBuffer, PipelineVariable, and OutVariable. For more information, see
+#         about_CommonParameters (https://go.microsoft.com/fwlink/?LinkID=113216).
+
+# INPUTS
+
+# OUTPUTS
+#     LiteDB.ILiteCollection[LiteDB.BsonDocument]
+#         The collection instance.
+
+
+#     -------------------------- EXAMPLE 1 --------------------------
+
+#     Use-LiteDatabase :memory: {
+#         # get the collection
+#         $MyCollection = Get-LiteCollection MyCollection
+
+#         # use it...
+#         $MyCollection
+#     }
+
+
+# RELATED LINKS
+#     New-LiteDatabase
+#     Use-LiteDatabase
+$dbConnectionString = New-DbConnectionString -Culture "en-US" -IgnoreCase -IgnoreNonSpace -IgnoreSymbols -ConnectionType Shared -AutoRebuild -FilePath "$env:TEMP\temp.db" -Upgrade
+$db = Initialize-DB -ConnectionString $dbConnectionString
+# $db = New-LiteDatabase -ConnectionString $connectionString
+# $db | Get-Member -MemberType Method -Force | ft
+# Invoke-LiteCommand 'select pragmas from $database;' -Database $db
+# Ensure-LiteDBCollection -Database $db -CollectionName 'Temp' -Indexes @(
+#         [PSCustomObject]@{ Field='Hash'; Unique=$true }
+# )
+
+
+# $CollectionName = "Temp"
+# $Collection = Get-LiteCollection -CollectionName $CollectionName -AutoId ObjectId -Database $db
+# $docs = $Collection | Get-Member -MemberType Method -Force 
+# $docs
+# $Collection.get_AutoId()
+# EnsureIndex(string name, LiteDB.BsonExpression expression, bool unique = False)
+# $TempId = [LiteDB.ObjectId]::NewObjectId()
+# $TempDoc = @{ _id = $TempId; TempField = 'Temp' }
+# Add-LiteData -Collection $Collection -InputObject $TempDoc
+# Ensure-LiteDBCollection -Database $db -CollectionName $CollectionName -Indexes @(
+#         [PSCustomObject]@{ Field='Hash'; Unique=$true },
+#         [PSCustomObject]@{ Field='Guid'; Unique=$false}
+# )
+
+# Test-LiteDBCollection -Database $db -CollectionName $CollectionName -Indexes @(
+#         [PSCustomObject]@{ Field='Hash'; Unique=$true},
+#         [PSCustomObject]@{ Field='Guid'; Unique=$false}
+# )
+# Initialize-Collections -Database $db | Out-Null
+# $docs = $Collection | Get-Member -MemberType Method -Force 
+# $docs | ft
+# remove-item "$env:TEMP\temp.db"
+# $connectionString | Get-Member -MemberType Method -Force | ft
+# [LiteDB.ConnectionString].GetMethods() | ft
---
+++ scratch.ps1
@@ -0,0 +1,294 @@
+using module ".\ps-migration.psd1"
+$dbPath = ".\StoredObjects\ps-migration.db"
+ 
+Remove-Item -Path $dbPath
+Clear-Host
+Import-Module .\ps-migration.psd1 -Force
+Clear-Host
+$dbConnectionString = New-DbConnectionString -Culture "en-US" -IgnoreCase -IgnoreNonSpace -IgnoreSymbols -ConnectionType Shared -AutoRebuild -FilePath $dbPath -Upgrade
+Initialize-DB -ConnectionString $dbConnectionString
+$db = New-LiteDatabase -ConnectionString $dbConnectionString
+
+$collection = Get-LiteCollection -CollectionName "Temp" -Database $db
+$collection
+
+
+Write-Host "== hashdoc1"
+$hashdoc1 = @{
+    name = 'hashdoc1'
+    test = 'data'
+    dock = "inforamtion"
+    store = @(1, 2, 3)
+}
+$hashdoc1
+
+Write-Host "++ hash1"
+$hash1 = Get-DataHash -DataObject $hashdoc1
+$hashdoc1['Hash'] = $hash1.Hash
+$hashdoc1
+
+Write-Host "== hashdoc2"
+$hashdoc2 = [PSCustomObject]@{
+    name = 'hashdoc2'
+    test = 'data2'
+    dock = "inforamtion2"
+}
+$hashdoc2
+
+Write-Host "++ hash2"
+$hash2 = Get-DataHash -DataObject $hashdoc2
+($hashdoc2 | Add-Member -MemberType NoteProperty -Name "Hash" -Value $hash2.Hash -PassThru) | Out-Null
+$hashdoc2
+
+Write-Host "== hashdoc3"
+$hashdoc3 = [PSCustomObject]@{
+    name = 'hashdoc3'
+    test = 'data'
+    dock = "inforamtion"
+    store = @(1, 2, 3)
+    # "`$Ref" = "ref"
+}
+$hashdoc3
+
+# Write-Host "++ hash3"
+# $hash3 = Get-DataHash -DataObject $hashdoc3
+# ($hashdoc3 | Add-Member -MemberType NoteProperty -Name "Hash" -Value $hash3.Hash -PassThru) | Out-Null
+# $hashdoc3
+
+# Write-Host "== dbdoc1"
+# $hashdoc1, $hashdoc2 | Add-LiteData -Collection $collection
+
+# $dbdoc1 = Get-LiteData $collection -Where 'Hash = @Hash', @{Hash = $hashdoc1.Hash}
+# $dbdoc1
+
+# Write-Host "== dbdoc2"
+# $dbdoc2 = $hashdoc2.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+# $dbdoc2
+
+# Write-Host "== dbdoc2 data"
+# $dbdoc2 | Add-DbDocument -Database $db -Collection $collection
+
+Write-Host "== dbdoc3 data"
+$dbdoc3 = $hashdoc3 | Add-DbDocument -Database $db -Collection $collection
+$dbdoc3
+
+Write-Host "== dbdoc3 namechange"
+$nameChangeDoc = $dbdoc3.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$nameChangeDoc.name = "dbdoc3"
+$dbdoc3b = $nameChangeDoc | Add-DbDocument -Database $db -Collection $collection
+$dbdoc3
+$dbdoc3b
+
+Write-Host "== dbdoc3 datachange"
+$dataChangeDoc = $dbdoc3b.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dataChangeDoc.test = "Updated Data"
+$dbdoc3c = $dataChangeDoc | Add-DbDocument -Database $db -Collection $collection
+$dbdoc3b
+$dbdoc3c
+
+Write-Host "== Renew Objects from Db"
+$dbdoc3 = $dbdoc3.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc3b = $dbdoc3b.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc3c = $dbdoc3c.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc3
+$dbdoc3b
+$dbdoc3c
+
+Write-Host "== Get All Documents from Temp"
+Get-DbDocumentAll -Datbase $db -Collection $collection
+
+Write-Host "== Versions by Guid - Ldbc"
+$dbdoc3c.Guid
+$dbdoc3c.Guid | Get-DbDocumentVersionsByGuid -Database $db -Collection $collection
+
+Write-Host "== Get Next Version"
+$dbdoc3b
+$dbdoc3b.Hash | Get-DbDocumentVersion -Database $db -Collection $collection -Next
+
+Write-Host "== Get Previous Version"
+$dbdoc3b
+$dbdoc3b.Hash | Get-DbDocumentVersion -Database $db -Collection $collection -Previous
+
+Write-Host "== Get Latest Version"
+$dbdoc3b
+$latest = $dbdoc3b.Hash | Get-DbDocumentVersion -Database $db -Collection $collection -Latest
+$latest
+
+Write-Host "== Get Original Version"
+$dbdoc3b
+$original = $dbdoc3b.Hash | Get-DbDocumentVersion -Database $db -Collection $collection -Original
+$original
+
+Write-host "== Attempt Next, get latest"
+$latest.Hash | Get-DbDocumentVersion -Database $db -Collection $collection -Next
+
+Write-host "== Attempt Prebvious, get original"
+$original.Hash | Get-DbDocumentVersion -Database $db -Collection $collection -Previous
+
+Write-Host "== DbGuidRef"
+$dbdoc3
+New-DbGuidRef -DbDocument $dbdoc3 -Collection $collection
+
+Write-Host "== DbHashRef"
+$dbdoc3
+New-DbHashRef -DbDocument $dbdoc3 -Collection $collection
+
+Write-Host "== Allow Old hash in not lastest version"
+$dbdoc3
+$dbdoc5 = $dbdoc3 | Add-DbDocument -Database $db -Collection $collection
+$dbdoc5
+
+Write-Host "== Allow Old hash not in lastest version - again."
+$nameChangeDoc = $dbdoc3c.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$nameChangeDoc.name = "dbdoc3d"
+$dbdoc3d = $nameChangeDoc | Add-DbDocument -Database $db -Collection $collection
+$dbdoc3c
+$dbdoc3d
+$dbdoc6 = $dbdoc3 | Add-DbDocument -Database $db -Collection $collection
+$dbdoc6
+$dbdoc7 = $dbdoc3 | Add-DbDocument -Database $db -Collection $collection
+
+Write-Host "++ Getting Versions"
+$dbdoc7.Guid | Get-DbDocumentVersionsByGuid -Database $db -Collection $collection
+
+Write-Host "== Get-DbHashRef"
+$dbdoc7
+$dbdoc7 | Get-DbHashRef -Database $db -Collection $collection
+
+
+Write-Host "== Resolve List of mixed documents and refs"
+$Versions = $dbdoc7.Guid | Get-DbDocumentVersionsByGuid -Database $db -Collection $collection
+$Versions
+Write-Host "++ resolved list"
+$resolvedVersions = $dbdoc7.Guid | Get-DbDocumentVersionsByGuid -Database $db -Collection $collection -ResolveRefs
+$resolvedVersions
+
+Write-Host "== Renew Objects from Db"
+$dbdoc3 = $dbdoc3.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc3b = $dbdoc3b.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc3c = $dbdoc3c.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc3d = $dbdoc3d.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc5 = $dbdoc5.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc6 = $dbdoc6.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc7 = $dbdoc7.Hash | Get-DbDocumentByHash -Database $db -Collection $collection
+$dbdoc3
+$dbdoc3b
+$dbdoc3c
+$dbdoc3d
+$dbdoc5
+$dbdoc6
+$dbdoc7
+
+Write-Host "== Get All Documetns from collection - resolved"
+Get-DbDocumentAll -Datbase $db -Collection $collection -ResolveRefs
+
+Write-Host "== Resolving a Single by Hash"
+$dbdoc7
+$dbdoc7.Hash | Get-DbDocumentByHash -Database $db -Collection $collection -ResolveRefs
+
+Write-Host "== Resolving a Single by Id"
+$dbdoc7
+$dbdoc7._id | Get-DbDocumentById -Database $db -Collection $collection -ResolveRefs
+
+Write-Host "== Resolving Latest"
+$dbdoc3d.Hash | Get-DbDocumentVersion -Database $db -Collection $collection -Latest
+$dbdoc3d.Hash | Get-DbDocumentVersion -Database $db -Collection $collection -Latest -ResolveRefs
+
+Write-Host "== Testing the Class"
+$store = New-LiteDbAppendOnlyStore -Database $db -Collection $collection
+$store
+$store.GetType() | ft
+$store | Get-Member -MemberType Method -Force | ft
+
+Write-Host "== Class Create"
+Write-Host "++ hashdoc8"
+$hashdoc8 = [PSCustomObject]@{
+    name = 'hashdoc8'
+    data = 'test'
+    information = "dock"
+    store = @(1, "a", @{fun = "times"; number1 =1; list = @("i1", "i2", "i3")})
+    # "`$Ref" = "ref"
+}
+$hashdoc8 | fl
+Write-Host "++ Create"
+($hashdoc8 = $store.Create($hashdoc8)) | Out-Null
+$hashdoc8 | fl
+
+Write-Host "++ GetVersionsByGuid"
+($Versions = $store.GetVersionsByGuid($dbdoc7.Guid)) | Out-Null
+$Versions | ft
+
+Write-Host "++ GetVersionsByGuid - Resolve `$Refs"
+($Versions = $store.GetVersionsByGuid($dbdoc7.Guid, $true)) | Out-Null
+$Versions | ft
+
+Write-Host "++ ReadAll"
+($allTempDocs = $store.ReadAll()) | Out-Null
+$allTempDocs | ft
+
+Write-Host "++ ReadAll - Resolve `$Refs"
+($allTempDocsResolved = $store.ReadAll($true)) | Out-Null
+$allTempDocsResolved | ft
+
+Write-Host "++ ReadByHash"
+($dbdoc7_hashRead = $store.ReadByHash($dbdoc7.Hash)) | Out-Null
+$dbdoc7_hashRead | fl
+
+Write-Host "++ ReadByHash - Resolve `$Refs"
+($dbdoc7_hashRead = $store.ReadByHash($dbdoc7.Hash, $true)) | Out-Null
+$dbdoc7_hashRead | fl
+
+Write-Host "++ ReadById"
+($dbdoc7_idRead = $store.ReadById($dbdoc7._id)) | Out-Null
+$dbdoc7_idRead | fl
+
+Write-Host "++ ReadById - Resolve `$Refs"
+($dbdoc7_idRead = $store.ReadById($dbdoc7._id, $true)) | Out-Null
+$dbdoc7_idRead | fl
+
+Write-Host "++ ReadVersion - Original"
+($dbdoc3b_original = $store.ReadVersion($dbdoc3b.Hash, [dbVersionSteps]::Original)) | Out-Null
+$dbdoc3b_original | fl
+
+Write-Host "++ ReadVersion - Original - Resolve `$Refs"
+($dbdoc3b_original = $store.ReadVersion($dbdoc3b.Hash, [dbVersionSteps]::Original), $true) | Out-Null
+$dbdoc3b_original | fl
+
+Write-Host "++ ReadVersion - Latest"
+($dbdoc3b_original = $store.ReadVersion($dbdoc3b.Hash, [dbVersionSteps]::Latest)) | Out-Null
+$dbdoc3b_original | fl
+
+Write-Host "++ ReadVersion - Latest - Resolve `$Refs"
+($dbdoc3b_original = $store.ReadVersion($dbdoc3b.Hash, [dbVersionSteps]::Latest, $true)) | Out-Null
+$dbdoc3b_original | fl
+
+Write-Host "++ ReadVersion - Previous"
+($dbdoc3b_original = $store.ReadVersion($dbdoc3b.Hash, [dbVersionSteps]::Previous)) | Out-Null
+$dbdoc3b_original | fl
+
+Write-Host "++ ReadVersion - Previous - Resolve `$Refs"
+($dbdoc3b_original = $store.ReadVersion($dbdoc3b.Hash, [dbVersionSteps]::Previous, $true)) | Out-Null
+$dbdoc3b_original | fl
+
+Write-Host "++ ReadVersion - Next"
+($dbdoc3b_original = $store.ReadVersion($dbdoc3b.Hash, [dbVersionSteps]::Next)) | Out-Null
+$dbdoc3b_original | fl
+
+Write-Host "++ ReadVersion - Next - Resolve `$Refs"
+($dbdoc3b_original = $store.ReadVersion($dbdoc3b.Hash, [dbVersionSteps]::Next, $true)) | Out-Null
+$dbdoc3b_original | fl
+
+Write-Host "++ GetGuidRef"
+$newDbGuidRef = New-DbGuidRef -Collection $collection -DbDocument $dbdoc3
+($guidRef = $store.GetGuidRef($newDbGuidRef)) | Out-Null
+$guidRef
+
+Write-Host "++ GetHashRef"
+($hashRef = $store.GetHashRef($dbdoc7)) | Out-Null
+$hashRef
+
+Write-Host "++ Ensure Collection"
+$store.EnsureCollection("TestCollection", @(
+        [PSCustomObject]@{ Field='Hash'; Unique=$true }
+        [PSCustomObject]@{ Field="Guid"; Unique=$false}
+    ))
