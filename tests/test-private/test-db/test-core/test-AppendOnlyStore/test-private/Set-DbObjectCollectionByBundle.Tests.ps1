6e36569c54f9821a9fb6a18006255596ed227754
David Midlo <dmidlo@gmail.com>
2025-03-13 14:40:19 -0500

    Really solid test case for multiple insertions.

--- tests/test-private/test-db/test-core/test-AppendOnlyStore/test-private/Set-DbObjectCollectionByBundle.Tests.ps1
+++ tests/test-private/test-db/test-core/test-AppendOnlyStore/test-private/Set-DbObjectCollectionByBundle.Tests.ps1
@@ -1,5 +1,56 @@
 Describe "Set-DbObjectCollectionByBundle Integration Tests" -Tag 'Integration' {
     BeforeAll {
+        function SeedDatabase {
+            param($Database, $Collection, $Bundle, $BundleId)
+
+            # Seed test data
+            if (-not $BundleId) {
+                $BundleId = [Guid]::NewGuid()
+            }
+            foreach ($version in $Bundle) {
+                $AddParams = @{
+                    Database = [LiteDB.LiteDatabase]$Database
+                    Collection = [LiteDB.LiteCollection[LiteDB.BsonDocument]]$Collection
+                    Data = [PSCustomObject]@{DataField = $version; BundleId = $BundleId}
+                }
+                Add-DbDocument @AddParams
+            }
+
+        }
+
+        function SeedSet1 {
+            $bundles = [PSCustomObject]@{
+                $srcBundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData4","OriginalData5","OriginalData6","OriginalData7")
+                $srcGuid1 = [Guid]::NewGuid()
+                $srcBundle2 = @("OriginalData1","OriginalData2","OriginalData1","OriginalData2","OriginalData1","OriginalData2","OriginalData1")
+                $srcGuid2 = [Guid]::NewGuid()
+                $srcBundle3 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData1","OriginalData2")
+                $srcGuid3 = [Guid]::NewGuid()
+                $dstBundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData3","OriginalData4")
+                $dstGuid1 = [Guid]::NewGuid()
+                $dstBundle2 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData3","OriginalData1")
+                $dstGuid2 = [Guid]::NewGuid()
+                $dstBundle3 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData2","OriginalData4","OriginalData3","OriginalData5")
+                $dstGuid3 = [Guid]::NewGuid()
+            }
+
+            $props = $bundles.PSObject.Properties.Name
+            foreach ($prop in $props) {
+                $id = $prop[-1]
+
+                if ($prop -like '*srcBundle*') {
+                    $guidProp = "srcGuid$id"
+                    SeedDatabase -Database $db -Collection $SourceCollection -Bundle $bundles.$prop -BundleId $bundles.$guidProp
+                }
+                elseif ($prop -like '*dstBundle*') {
+                    $guidProp = "dstGuid$id"
+                    SeedDatabase -Database $db -Collection $DestCollection -Bundle $bundles.$prop -BundleId $bundles.$guidProp
+                }
+            }
+
+            return $bundles
+        }
+
         $dbpath = "$env:Temp\temp.db"
         if(Test-Path -Path $dbpath) {
             Remove-Item -Path $dbpath
@@ -23,125 +74,113 @@ Describe "Set-DbObjectCollectionByBundle Integration Tests" -Tag 'Integration' {
         $RefCollection = Get-LiteCollection -Database $db -CollectionName 'RefCol'
     }
 
-    # AfterEach {
-    #     Remove-Item -Path "$env:Temp\temp.db"
-    # }
+
     Context "Set-DbObjectCollectionByBundle: Atomic Tests" {
+        AfterEach {
+            Remove-Item -Path "$env:Temp\temp.db"
+        }
 
         $TC01 = @{
             Name = "Set-DbOjectCollectionByBundle TC01: Generates ContentMark, VersionId, BundleId, and persists correctly"
             Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
             Test = {
                 # Seed test data
-                $Bundle1Id = [Guid]::NewGuid()
                 $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData4","OriginalData5","OriginalData6","OriginalData7")
-                foreach ($version in $bundle1) {
-                    $AddParams = @{
-                        Database = [LiteDB.LiteDatabase]$db
-                        Collection = [LiteDB.LiteCollection[LiteDB.BsonDocument]]$SourceCollection
-                        Data = [PSCustomObject]@{DataField = $version; BundleId = $Bundle1Id}
-                    }
-                    Add-DbDocument @AddParams
-                }
+
+                $BundleId = [Guid]::NewGuid()
+                SeedDatabase -Database $db -Collection $SourceCollection -Bundle $bundle1 -BundleId $BundleId
+                $bundle1 = Get-DbDocumentVersionsByBundle -Database $db -Collection $SourceCollection -BundleId $BundleId
+
+
+                $bundle1.Count | Should -Be 7
+                $VersionIds = $bundle1 | Select-Object -ExpandProperty VersionId | Sort-Object -Unique
+                $VersionIds.Count | Should -Be 7
+                $BundleIds = $bundle1 | Select-Object -ExpandProperty BundleId | Sort-Object -Unique
+                $BundleIds.Count | Should -Be 1
+                $ContentMarks = $bundle1 | Select-Object -ExpandProperty ContentMark | Sort-Object -Unique
+                $ContentMarks.Count | Should -Be 7
+                $bundle1[0] | Should -BeNullOrEmpty
+
+                $bundle1[6].PSObject.Properties.Name | Should -Not -Contain '$Ref'
+                $bundle1[6].'$ObjVer' | Should -Be 1
+                $bundle1[6].DataField | Should -Be 'OriginalData1'
+                $bundle1[5].PSObject.Properties.Name | Should -Not -Contain '$Ref'
+                $bundle1[5].'$ObjVer' | Should -Be 2
+                $bundle1[5].DataField | Should -Be 'OriginalData2'
+                $bundle1[4].PSObject.Properties.Name | Should -Not -Contain '$Ref'
+                $bundle1[4].'$ObjVer' | Should -Be 3
+                $bundle1[4].DataField | Should -Be 'OriginalData3'
+                $bundle1[3].PSObject.Properties.Name | Should -Not -Contain '$Ref'
+                $bundle1[3].'$ObjVer' | Should -Be 4
+                $bundle1[3].DataField | Should -Be 'OriginalData4'
+                $bundle1[2].PSObject.Properties.Name | Should -Not -Contain '$Ref'
+                $bundle1[2].'$ObjVer' | Should -Be 5
+                $bundle1[2].DataField | Should -Be 'OriginalData5'
+                $bundle1[1].PSObject.Properties.Name | Should -Not -Contain '$Ref'
+                $bundle1[1].'$ObjVer' | Should -Be 6
+                $bundle1[1].DataField | Should -Be 'OriginalData6'
+                $bundle1[0].PSObject.Properties.Name | Should -Not -Contain '$Ref'
+                $bundle1[0].'$ObjVer' | Should -Be 7
+                $bundle1[0].DataField | Should -Be 'OriginalData7'
             }
         }
-        # It @TC01
-    }
+        It @TC01
 
-    $TC02 = @{
-        Name = "Set-DbOjectCollectionByBundle TC02: Generates ContentMark, VersionId, BundleId, and persists correctly"
-        Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
-        Test = {
-            # Seed test data
-            $Bundle1Id = [Guid]::NewGuid()
-            $bundle1 = @("OriginalData1","OriginalData2","OriginalData1","OriginalData2","OriginalData1","OriginalData2","OriginalData1")
-            foreach ($version in $bundle1) {
-                $AddParams = @{
-                    Database = [LiteDB.LiteDatabase]$db
-                    Collection = [LiteDB.LiteCollection[LiteDB.BsonDocument]]$SourceCollection
-                    Data = [PSCustomObject]@{DataField = $version; BundleId = $Bundle1Id}
-                }
-                Add-DbDocument @AddParams
+        $TC02 = @{
+            Name = "Set-DbOjectCollectionByBundle TC02: Generates ContentMark, VersionId, BundleId, and persists correctly"
+            Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
+            Test = {
+                # Seed test data
+                $bundle1 = @("OriginalData1","OriginalData2","OriginalData1","OriginalData2","OriginalData1","OriginalData2","OriginalData1")
+                SeedDatabase -Database $db -Collection $SourceCollection -Bundle $bundle1
             }
         }
+        It @TC02
+
+        $TC03 = @{
+            Name = "Set-DbOjectCollectionByBundle TC03: Generates ContentMark, VersionId, BundleId, and persists correctly"
+            Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
+            Test = {
+                # Seed test data
+                $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData1","OriginalData2")
+                SeedDatabase -Database $db -Collection $SourceCollection -Bundle $bundle1
+            }
+        }
+        It @TC03
+
+        $TC04 = @{
+            Name = "Set-DbOjectCollectionByBundle TC04: Generates ContentMark, VersionId, BundleId, and persists correctly"
+            Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
+            Test = {
+                # Seed test data
+                $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData3","OriginalData4")
+                SeedDatabase -Database $db -Collection $DestCollection -Bundle $bundle1
+            }
+        }
+        It @TC04
+        
+        $TC05 = @{
+            Name = "Set-DbOjectCollectionByBundle TC05: Generates ContentMark, VersionId, BundleId, and persists correctly"
+            Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
+            Test = {
+                # Seed test data
+                $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData3","OriginalData1")
+                SeedDatabase -Database $db -Collection $DestCollection -Bundle $bundle1
+            }
+        }
+        It @TC05
+        
+        $TC06 = @{
+            Name = "Set-DbOjectCollectionByBundle TC06: Generates ContentMark, VersionId, BundleId, and persists correctly"
+            Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
+            Test = {
+                # Seed test data
+                $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData2","OriginalData4","OriginalData3","OriginalData5")
+                SeedDatabase -Database $db -Collection $DestCollection -Bundle $bundle1
+            }
+        }
+        It @TC06
     }
-    It @TC02
-
-#     $TC03 = @{
-#         Name = "Set-DbOjectCollectionByBundle TC03: Generates ContentMark, VersionId, BundleId, and persists correctly"
-#         # Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
-#         Test = {
-#             # Seed test data
-#             $Bundle1Id = [Guid]::NewGuid()
-#             $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData1","OriginalData2")
-#             foreach ($version in $bundle1) {
-#                 $AddParams = [PSCustomObject]@{
-#                     Database = $db
-#                     Collection = $SourceCollection
-#                     Data = @{DataField = $version; BundleId = $Bundle1Id}
-#                 }
-#                 Add-DbDocument @AddParams
-#             }
-#         }
-#     }
-#     It @TC03
-
-#     $TC04 = @{
-#         Name = "Set-DbOjectCollectionByBundle TC04: Generates ContentMark, VersionId, BundleId, and persists correctly"
-#         # Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
-#         Test = {
-#             # Seed test data
-#             $Bundle1Id = [Guid]::NewGuid()
-#             $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData3","OriginalData4")
-#             foreach ($version in $bundle1) {
-#                 $AddParams = [PSCustomObject]@{
-#                     Database = $db
-#                     Collection = $SourceCollection
-#                     Data = @{DataField = $version; BundleId = $Bundle1Id}
-#                 }
-#                 Add-DbDocument @AddParams
-#             }
-#         }
-#     }
-#     It @TC04
-
-#     $TC05 = @{
-#         Name = "Set-DbOjectCollectionByBundle TC05: Generates ContentMark, VersionId, BundleId, and persists correctly"
-#         # Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
-#         Test = {
-#             # Seed test data
-#             $Bundle1Id = [Guid]::NewGuid()
-#             $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData1","OriginalData2","OriginalData3","OriginalData1")
-#             foreach ($version in $bundle1) {
-#                 $AddParams = [PSCustomObject]@{
-#                     Database = $db
-#                     Collection = $SourceCollection
-#                     Data = @{DataField = $version; BundleId = $Bundle1Id}
-#                 }
-#                 Add-DbDocument @AddParams
-#             }
-#         }
-#     }
-#     It @TC05
-
-#     $TC06 = @{
-#         Name = "Set-DbOjectCollectionByBundle TC06: Generates ContentMark, VersionId, BundleId, and persists correctly"
-#         # Tag = @('private','db','dbCore','AppendOnlyStore','SetDbObjectCollectionByBundle','active')
-#         Test = {
-#             # Seed test data
-#             $Bundle1Id = [Guid]::NewGuid()
-#             $bundle1 = @("OriginalData1","OriginalData2","OriginalData3","OriginalData2","OriginalData4","OriginalData3","OriginalData5")
-#             foreach ($version in $bundle1) {
-#                 $AddParams = [PSCustomObject]@{
-#                     Database = $db
-#                     Collection = $SourceCollection
-#                     Data = @{DataField = $version; BundleId = $Bundle1Id}
-#                 }
-#                 Add-DbDocument @AddParams
-#             }
-#         }
-#     }
-#     It @TC06
 }
 #     BeforeEach {
 #         # Reset collections
